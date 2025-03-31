2e045ddf232fc78e4c8dccd04fdfa9004b57d003
Collin McMahon <cmcmahon@werfen.com>
2025-03-31 11:51:19 -0400

    Initial Commit

---
+++ src/schema.ts
@@ -0,0 +1,22 @@
+import { createZeroSchema } from "drizzle-zero";
+import { definePermissions } from "@rocicorp/zero"
+import type { Row } from "@rocicorp/zero";
+import * as drizzleSchema from "$lib/server/db/schema";
+
+// Convert to Zero schema
+export const schema = createZeroSchema(drizzleSchema, {
+
+  tables: {
+    user: {
+      id: true,
+      name: true,
+      email: true,
+    }
+  }
+});
+
+// Define permissions with the inferred types from Drizzle
+export type Schema = typeof schema;
+export type User = Row<typeof schema.tables.user>;
+
+export const permissions = definePermissions(schema, () => ({}));
