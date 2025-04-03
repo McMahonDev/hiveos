954c7761b24c1eff06abf6858ea520b444ed2d5a
Collin McMahon <thecmcmahon0+dev@gmail.com>
2025-04-03 13:27:32 -0400

    Update Svelte configuration to support dynamic adapter selection based on deployment environment

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import { adapter as cf } from '@sveltejs/adapter-cloudflare';
+import { adapter as node } from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const deployment = import.meta.env.VITE_DEPLOYMENT;
+const adapter = deployment === 'node' ? node() : cf();
 const config = {
 	preprocess: vitePreprocess(),
 	kit: { adapter: adapter() }
