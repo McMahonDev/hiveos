52080c50c5f3691b91abf6d662fdbfb5332c3698
Collin McMahon <thecmcmahon0+dev@gmail.com>
2025-04-07 14:23:49 -0400

    Update userID in z.svelte.ts and ensure consistent formatting

--- src/lib/z.svelte.ts
+++ src/lib/z.svelte.ts
@@ -6,10 +6,10 @@ import { schema, type Schema } from '../schema';
 
 export function get_z_options() {
 	return {
-		userID: 'anon',
+		userID: 'uu1',
 		server: import.meta.env.VITE_CONNECTION_STRING,
 		schema,
-        kvStore: 'idb',
+		kvStore: 'idb'
 		// ... other options
 	} as const;
 }
