2ebc7b66eed93cc87af7ba1f50631e730bbc3f67
Collin McMahon <thecmcmahon0+dev@gmail.com>
2025-06-11 22:49:14 -0400

    fix: update Vite config to define DATABASE_URL using process.env

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		'import.meta.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL)
+	}
 });
