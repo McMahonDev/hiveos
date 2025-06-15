a01fa32d1a4cbac60715017b2437bec840df94ae
Collin McMahon <thecmcmahon0+dev@gmail.com>
2025-06-14 21:39:07 -0400

    fix: enhance logging for user authentication flow in layout server

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -14,14 +14,19 @@ export async function load({ locals, url, cookies }: { locals: Locals; url: URL;
 	let auth: boolean;
 	let id: string;
 	let groupId: string;
+	console.log('Loading layout server');
 	if (!locals?.user) {
+		console.log('No user found, redirecting to login');
 		auth = false;
 		id = '';
 		groupId = '0';
 		if (url.pathname !== '/account/login' && url.pathname !== '/account/register') {
+			console.log('Redirecting to login page');
+			console.log('Current URL:', url.pathname);
 			throw redirect(302, '/account/login');
 		}
 	} else {
+		console.log('User found, setting auth to true');
 		auth = true;
 		id = locals.user.id;
 		groupId = locals.user.groupId;
