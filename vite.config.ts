b0fbcdd8369706955a27956d15520ddee4592fbc
Collin McMahon <cmcmahon@werfen.com>
2025-07-21 15:32:10 -0400

    refactor: switch from Cloudflare adapter to Node adapter and remove Cloudflare Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { cloudflare } from '@cloudflare/vite-plugin';
 
 export default defineConfig({
 	// plugins: [sveltekit(), cloudflare()]
